716e9bcc2e45ea5c27cef51a6e5c82ed1da02071
JAGASABARIVEL <32729740+JAGASABARIVEL@users.noreply.github.com>
2023-03-09 21:00:41 +0530

    Delete Capstone Projects/Movieplan Project directory

--- Capstone Projects/Movieplan Project/be/MyMoviewPlan/src/main/java/com/mymovieplan/repository/CartRepo.java
+++
@@ -1,9 +0,0 @@
-package com.mymovieplan.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mymovieplan.entity.Cart;
-
-public interface CartRepo extends JpaRepository<Cart, Integer> {
-
-}
--- Capstone Projects/Movieplan Project/be/MyMoviewPlan/src/main/java/com/mymovieplan/repository/MovieRepo.java
+++
@@ -1,8 +0,0 @@
-package com.mymovieplan.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mymovieplan.entity.Movie;
-
-public interface MovieRepo extends JpaRepository<Movie, Integer> {
-}
